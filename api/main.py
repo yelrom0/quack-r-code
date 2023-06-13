c8895a44d94b9fed5858a0690b5225c314ce85e2
Daniel Morley <yelrom0@gmail.com>
2023-06-13 22:53:19 +1000

    feat(Main): full endpoint

--- api/main.py
+++ api/main.py
@@ -10,6 +10,7 @@ from fastapi.responses import Response
 from api.open_ai_api import AIApi
 from api.qr_code import QRAPI
 from api.meta.schemas import GPTPrompt, QRCode
+from api.stable_diffusion_api import StableDiffusionAPI
 
 # create main server
 app = FastAPI(
@@ -30,17 +31,43 @@ async def test_chatgpt_prompt(payload: GPTPrompt) -> str:
 
 @app.post(
     "/test-qr",
-    responses={
-        200: {
-            "content": {"image/png": {}}
-        }
-    },
+    responses={200: {"content": {"image/png": {}}}},
     response_class=Response,
 )
 def test_qr_code(payload: QRCode) -> Response:
-    # setup api and send qr code representing the url back to client
+    """setup api and send qr code representing the url back to client"""
     qr_api = QRAPI(url=payload.url)
     return Response(
         content=qr_api.get_qr_code_as_bytes(),
+        media_type="image/png",
+    )
+
+
+@app.post(
+    "/create-qr",
+    responses={200: {"content": {"image/png": {}}}},
+    response_class=Response,
+)
+async def create_qr_code(
+    qr_data: QRCode,
+    prompt: GPTPrompt,
+) -> Response:
+    """Generate fancy AI QR code using the prompt and url provided"""
+    gpt_api = AIApi()
+    resp: list[str] = await gpt_api.get_stable_diffusion_prompt(
+        prompt.user_prompt,
+    )
+    qr_api = QRAPI(
+        url=qr_data.url,
+    )
+    diffusion_api = StableDiffusionAPI()
+    diffusion_api.convert_qr(
+        qr=qr_api,
+        prompt=resp[0],
+        negative_prompt=resp[1],
+        steps=20,
+    )
+    return Response(
+        content=qr_api.post_ai_img,
         media_type="image/png",
     )
